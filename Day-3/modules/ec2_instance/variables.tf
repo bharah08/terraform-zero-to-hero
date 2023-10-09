51ecf8c5cbb89793698843ad86f799480305d6d7
bharah08 <111727649+bharah08@users.noreply.github.com>
2023-10-09 17:00:02 +0530

    Update variables.tf

--- Day-3/modules/ec2_instance/variables.tf
+++ Day-3/modules/ec2_instance/variables.tf
@@ -1,7 +1,11 @@
 variable "ami_value" {
     description = "value for the ami"
+    type= string
+    default = "ami-0f5ee92e2d63afc18"
 }
 
 variable "instance_type_value" {
     description = "value for instance_type"
+    type = string
+    default = "t2.micro"
 }
